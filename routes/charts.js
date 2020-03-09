3c107e7e1f29ee13028c900daf9954e964156290
artemustilov <artem.ustilov@gmail.com>
2020-03-09 11:19:17 +0200

    variant 2 handling

--- routes/charts.js
+++ routes/charts.js
@@ -5,18 +5,18 @@ const pgp = require("pg-promise")();
 const HOST = '146.148.7.100';
 const DB = 'thesis';
 
-router.get('/', function(req, res, next) {
-  const { user, password, l, n, sel_type, table, run_id=0, offset=0, limit=100 } = req.query || {};
+router.get('/', function (req, res, next) {
+  const {user, password, l, n, sel_type, table, run_id = 0, offset = 0, type, limit = 100} = req.query || {};
   res.set('Access-Control-Allow-Origin', '*');
-  try{
+  try {
     const db = pgp({
       host: HOST,
       database: DB,
       user,
       password
     });
-    db.any("SELECT * FROM $1:name WHERE l = $2 AND n = $3 AND sel_type = $4 AND run_id = $5 ORDER BY iteration ASC LIMIT $6 OFFSET $7",
-      [table, l, n, sel_type, run_id, limit, offset])
+    db.any(`SELECT * FROM $1:name WHERE ${n ? 'n = $2' : 'size_pop_type = $2'} AND l = $3 AND sel_type = $4 AND run_id = $5 ORDER BY iteration ASC LIMIT $6 OFFSET $7`,
+      [table, n || type, l, sel_type, run_id, limit, offset])
       .then(function (data) {
         res.send(data);
       })
@@ -24,7 +24,7 @@ router.get('/', function(req, res, next) {
         res.send(error);
         console.error("ERROR:", error);
       });
-  }catch (e) {
+  } catch (e) {
     res.send(e);
   }
 });
