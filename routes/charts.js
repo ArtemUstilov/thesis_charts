367e809d671100abbba3da73ab26ad14a4a1a31d
artemustilov <artem.ustilov@gmail.com>
2020-03-18 13:46:35 +0200

    select init

--- routes/charts.js
+++ routes/charts.js
@@ -6,7 +6,7 @@ const HOST = '146.148.7.100';
 const DB = 'thesis';
 
 router.get('/', function (req, res, next) {
-  const {user, password, l, n, sel_type, table, run_id = 0, offset = 0, type, limit = 100} = req.query || {};
+  const {user, password, l, n, sel_type, table, run_id = 0, offset = 0, type, init='all_0',limit = 100} = req.query || {};
   res.set('Access-Control-Allow-Origin', '*');
   try {
     const db = pgp({
@@ -15,8 +15,8 @@ router.get('/', function (req, res, next) {
       user,
       password
     });
-    db.any(`SELECT * FROM $1:name WHERE ${n ? 'n = $2' : 'size_pop_type = $2'} AND l = $3 AND sel_type = $4 AND run_id = $5 ORDER BY iteration ASC LIMIT $6 OFFSET $7`,
-      [table, n || type, l, sel_type, run_id, limit, offset])
+    db.any(`SELECT * FROM $1:name WHERE ${n ? 'n = $2' : 'size_pop_type = $2'} AND l = $3  AND init = $8 AND sel_type = $4 AND run_id = $5 ORDER BY iteration ASC LIMIT $6 OFFSET $7`,
+      [table, n || type, l, sel_type, run_id, limit, offset, init])
       .then(function (data) {
         res.send(data);
       })
